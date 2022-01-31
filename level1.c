8c7ac3f953edf763f1de5482ffe930c08bf19f60
kevinB <59924989+kevbiss@users.noreply.github.com>
2022-01-31 21:02:48 +0100

    Create level1.c

---
+++ level1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int secret = 0x12341234;
+  char name[100] ={0};
+  read (0,name, 0x100);
+  if (secret != 0X12341234)
+  puts ("the variable was changed successfully")
+  puts("You have passed the ctf congratulations")
+  }else{
+  put("Error the variable wasn't change")
+}
+}
