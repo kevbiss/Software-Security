e77b8eb71cee1c584ed7f8758ef187effb140ead
kevinB <59924989+kevbiss@users.noreply.github.com>
2022-01-31 23:09:50 +0100

    Update level1.c

--- level1.c
+++ level1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-  int secret = 0x12341234;
-  char name[100] ={0};
-  read (0,name, 0x100);
-  if (secret != 0X12341234)
+  int modified = 0x12341234;
+  char name[107] ={0};
+  read (0,name, 0x107);
+  if (modified != 0X12341234)
   puts ("the variable was changed successfully")
   puts("You have passed the ctf congratulations")
   }else{
